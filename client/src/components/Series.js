0aaef064d84639009920bf95abe5aaa1ebf66025
Stanly Sukmajaya Kwok <stanlyskwok@gmail.com>
2021-03-13 23:18:15 +0700

    bagusin client

--- client/src/components/Series.js
+++ client/src/components/Series.js
@@ -3,7 +3,7 @@
 function Series(props) {
   const data = props.data;
   return (
-    <div style={{ display: 'inline-block' }}>
+    <div style={{ display: 'inline-block' }} className="pb-3">
       <img
         className="d-inline-block mr-3 ml-2 mb-3 mt-3 movie-img"
         style={{ width: '250px', height: '156px', textAlign: 'center' }}
@@ -11,9 +11,9 @@ function Series(props) {
         alt="First slide"
         />
       <h4 className="text-light mb-3">{data.title}</h4>
-      <button className="btn btn-danger mr-3"><i className="fas fa-heart"></i></button>
-      <button className="btn btn btn-secondary mr-3"><i class="fas fa-edit"></i></button>
-      <button className="btn btn-warning"><i className="fas fa-trash-alt"></i></button>
+      <button className="btn btn-icon btn-danger mr-3"><i className="fas fa-heart"></i></button>
+      <button className="btn btn-icon btn btn-primary mr-3"><i class="fas fa-edit"></i></button>
+      <button className="btn btn-icon btn-warning"><i className="fas fa-trash-alt"></i></button>
     </div>  
   )
 }
