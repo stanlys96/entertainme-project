cc3892d4ddb12350fbb3b07ef4f7c469566f0bb7
Stanly Sukmajaya Kwok <stanlyskwok@gmail.com>
2021-03-17 14:23:08 +0700

    add docker files

--- server/services/movies/routes/index.js
+++ server/services/movies/routes/index.js
@@ -4,5 +4,8 @@ const router = express.Router();
 const moviesRoutes = require('./movies');
 
 router.use('/movies', moviesRoutes);
+router.get('/', (req, res) => {
+  res.send('Hello!')
+})
 
 module.exports = router;
